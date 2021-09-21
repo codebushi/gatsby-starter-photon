46dd158bed2e0f4d0ce625b243f8dfd3e8aa5c37
Amine <34844409+freemanbot@users.noreply.github.com>
2021-09-21 11:32:48 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,9 @@ class Header extends React.Component {
             <section id="header">
                 <div className="inner">
                     <span className="icon major fa-cloud"></span>
-                    <h1>Hi, I'm <strong>Photon</strong>, another fine<br />
-                    little freebie from <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                    <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
-                    lobortis feugiat sapien sed etiam volutpat accumsan.</p>
+                    <h1>Hi, I'm <strong>Edukatek</strong>, another fine<br />
+                    IT related e-learning platfrom.
+                    <p>High quality trainings from our best cybersecurity experts.</p>
                     <ul className="actions">
                         <li><a href="#one" className="button scrolly">Discover</a></li>
                     </ul>
